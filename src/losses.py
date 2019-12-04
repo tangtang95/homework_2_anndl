cbe17b352617ae597a04a4aa1576ef4565f895d5
Tang-tang Zhou <tangtang.zhou@mail.polimi.it>
2019-12-05 00:44:04 +0100

    Add more losses

--- src/losses.py
+++ src/losses.py
@@ -2,10 +2,39 @@ import tensorflow as tf
 
 
 def dice_loss(y_true, y_pred):
+    """
+    The Dice coefficient is similar to the Jaccard Index (Intersection over Union, IoU).
+        DC = 2TP/(2TP+FP+FN)
+    The dice loss is defined as  1 - dice coefficient
+    """
     numerator = 2 * tf.reduce_sum(y_true * y_pred, axis=-1)
     denominator = tf.reduce_sum(y_true + y_pred, axis=-1)
 
     return 1 - (numerator + 1) / (denominator + 1)
+
+
+def dice_loss_scalar(y_true, y_pred):
+    """
+    The Dice loss similar to the above one, but in this case it returns a scalar instead of a tensor
+    """
+    numerator = 2 * tf.reduce_sum(y_true * y_pred, axis=(1,2,3))
+    denominator = tf.reduce_sum(y_true + y_pred, axis=(1,2,3))
+
+    return 1 - numerator / denominator
+
+
+def tversky_loss(beta):
+    """
+    Tversky index (TI) is a generalization of Dice’s coefficient. TI adds a weight to FP (false positives)
+    and FN (false negatives).
+    """
+    def loss(y_true, y_pred):
+        numerator = tf.reduce_sum(y_true * y_pred, axis=-1)
+        denominator = y_true * y_pred + beta * (1 - y_true) * y_pred + (1 - beta) * y_true * (1 - y_pred)
+
+        return 1 - (numerator + 1) / (tf.reduce_sum(denominator, axis=-1) + 1)
+
+    return loss
 
 
 def focal_loss(alpha=0.25, gamma=2):
@@ -50,3 +79,110 @@ def weighted_cross_entropy(beta):
         return tf.reduce_mean(loss)
 
     return loss
+
+
+def balanced_cross_entropy(beta):
+    """
+    Balanced cross entropy (BCE) is similar to WCE. The only difference is that we weight also the negative examples.
+    """
+    def convert_to_logits(y_pred):
+        y_pred = tf.clip_by_value(y_pred, tf.keras.backend.epsilon(), 1 - tf.keras.backend.epsilon())
+
+        return tf.math.log(y_pred / (1 - y_pred))
+
+    def loss(y_true, y_pred):
+        y_pred = convert_to_logits(y_pred)
+        pos_weight = beta / (1 - beta)
+        loss = tf.nn.weighted_cross_entropy_with_logits(logits=y_pred, labels=y_true, pos_weight=pos_weight)
+
+        # or reduce_sum and/or axis=-1
+        return tf.reduce_mean(loss * (1 - beta))
+
+    return loss
+
+
+def lovasz_softmax(y_true, y_pred):
+    """
+    Lovasz surrogate error for IoM: it should work with last CNN with linear activation, but it does not seem to work
+    """
+    return lovasz_hinge(labels=y_true, logits=y_pred)
+
+
+# --------------------------- LOVASZ - BINARY LOSSES ---------------------------
+# Lovasz-Softmax and Jaccard hinge loss in Tensorflow
+# Maxim Berman 2018 ESAT-PSI KU Leuven (MIT License)
+
+def lovasz_grad(gt_sorted):
+    """
+    Computes gradient of the Lovasz extension w.r.t sorted errors
+    See Alg. 1 in paper
+    """
+    gts = tf.reduce_sum(gt_sorted)
+    intersection = gts - tf.cumsum(gt_sorted)
+    union = gts + tf.cumsum(1. - gt_sorted)
+    jaccard = 1. - intersection / union
+    jaccard = tf.concat((jaccard[0:1], jaccard[1:] - jaccard[:-1]), 0)
+    return jaccard
+
+
+def lovasz_hinge(logits, labels, per_image=True, ignore=None):
+    """
+    Binary Lovasz hinge loss
+      logits: [B, H, W] Variable, logits at each pixel (between -\infty and +\infty)
+      labels: [B, H, W] Tensor, binary ground truth masks (0 or 1)
+      per_image: compute the loss per image instead of per batch
+      ignore: void class id
+    """
+    if per_image:
+        def treat_image(log_lab):
+            log, lab = log_lab
+            log, lab = tf.expand_dims(log, 0), tf.expand_dims(lab, 0)
+            log, lab = flatten_binary_scores(log, lab, ignore)
+            return lovasz_hinge_flat(log, lab)
+        losses = tf.map_fn(treat_image, (logits, labels), dtype=tf.float32)
+        loss = tf.reduce_mean(losses)
+    else:
+        loss = lovasz_hinge_flat(*flatten_binary_scores(logits, labels, ignore))
+    return loss
+
+
+def lovasz_hinge_flat(logits, labels):
+    """
+    Binary Lovasz hinge loss
+      logits: [P] Variable, logits at each prediction (between -\infty and +\infty)
+      labels: [P] Tensor, binary ground truth labels (0 or 1)
+      ignore: label to ignore
+    """
+
+    def compute_loss():
+        labelsf = tf.cast(labels, logits.dtype)
+        signs = 2. * labelsf - 1.
+        errors = 1. - logits * tf.stop_gradient(signs)
+        errors_sorted, perm = tf.nn.top_k(errors, k=tf.shape(errors)[0], name="descending_sort")
+        gt_sorted = tf.gather(labelsf, perm)
+        grad = lovasz_grad(gt_sorted)
+        loss = tf.tensordot(tf.nn.relu(errors_sorted), tf.stop_gradient(grad), 1, name="loss_non_void")
+        return loss
+
+    # deal with the void prediction case (only void pixels)
+    loss = tf.cond(tf.equal(tf.shape(logits)[0], 0),
+                   lambda: tf.reduce_sum(logits) * 0.,
+                   compute_loss,
+                   name="loss"
+                   )
+    return loss
+
+
+def flatten_binary_scores(scores, labels, ignore=None):
+    """
+    Flattens predictions in the batch (binary case)
+    Remove labels equal to 'ignore'
+    """
+    scores = tf.reshape(scores, (-1,))
+    labels = tf.reshape(labels, (-1,))
+    if ignore is None:
+        return scores, labels
+    valid = tf.not_equal(labels, ignore)
+    vscores = tf.boolean_mask(scores, valid, name='valid_scores')
+    vlabels = tf.boolean_mask(labels, valid, name='valid_labels')
+    return vscores, vlabels
